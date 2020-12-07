6e8aee56bec3a08352bf33bbe0f4f5751c9d63f1
Kirill Tyurin <kirill.tiurin@stm-labs.ru>
2020-12-07 22:07:08 +0300

    init

---
+++ src/components/App/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container, Logo } from './styled'
+
+export default function Header() {
+  return (
+    <Container>
+      <Logo>Neural Network</Logo>
+    </Container>
+  )
+}
---
+++ src/components/App/Header/styled.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  display: flex;
+  flex-flow: row nowrap;
+  padding: 10px;
+  background-color: #161616;
+`
+
+export const Logo = styled.div`
+  margin-left: 20px;
+  color: #fff;
+`
